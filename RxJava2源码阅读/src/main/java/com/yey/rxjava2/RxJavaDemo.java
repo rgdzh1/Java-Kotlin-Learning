f6bfbb77468ab60fc4fd0a4283482807a8feb2fe
Home <398049343@qq.com>
2020-06-07 10:43:35 +0800

    RxJava

--- RxJava2源码阅读/src/main/java/com/yey/rxjava2/RxJavaDemo.java
+++ RxJava2源码阅读/src/main/java/com/yey/rxjava2/RxJavaDemo.java
@@ -10,7 +10,9 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import sun.rmi.runtime.Log;
 
 
 public class RxJavaDemo {
@@ -80,6 +82,70 @@ public class RxJavaDemo {
                     public void onNext(Integer integer) {
                         System.out.println("onNext: " + integer);
                         System.out.println("观察者接收数据线程: " + Thread.currentThread().getId());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+    @Test
+    public void just操作符(){
+        Observable.just(1, 2, 3,4,5,6,7,8,9,10)
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println("onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+    }
+
+
+
+
+
+    @Test
+    public void map操作符() {
+        Observable.just(1, 2, 3)
+                // 可以将被观察者发送的数据类型转变成其他的类型
+                .map(new Function< Integer, String >() {
+                    @Override
+                    public String apply(Integer integer) throws Exception {
+                        return "I'm " + integer;
+                    }
+                })
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("onSubscribe");
+                        // 取消订阅后,ObservableEmitter发出的消息,观察者Observer将不会再收到.
+                        d.dispose();
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.println("onNext: " + s);
                     }
 
                     @Override
