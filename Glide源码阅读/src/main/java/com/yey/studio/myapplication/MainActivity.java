2bca6211d7ecbe8a85f406d5a8705f615a7a6130
Yey <rgdzh1@gmail.com>
2020-06-08 18:41:45 +0800

    Retrofit重新读

---
+++ Glide源码阅读/src/main/java/com/yey/studio/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.yey.studio.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView iv;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv = (ImageView) findViewById(R.id.iv);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn) {
+            Glide.with(this)
+                    .load("https://profile.csdnimg.cn/1/5/1/1_moliao2046")
+                    .into(iv);
+        }
+    }
+}
--- Retrofit源码阅读/src/main/java/com/yey/generic/RetrofitDemo.java
+++ Retrofit源码阅读/src/main/java/com/yey/generic/RetrofitDemo.java
@@ -1,57 +1,61 @@
 package com.yey.generic;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-
+import retrofit2.http.Query;
 
 
 public class RetrofitDemo {
-    interface GitHubService {
-        @GET("users/{user}/repos")
-        Call<Object> listRepos(@Path("user") String user);
+    interface MyService {
+        @GET("/App/Index/main")
+        Observable<Object> getUser(@Query("user") String user);
     }
+
     public static void main(String[] args) {
-        Retrofit retrofit =
-                new Retrofit.Builder()
-                        .baseUrl("https://api.github.com/")
-                        .client(new OkHttpClient()
-                                .newBuilder()
-                                .addInterceptor(new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-                                    @Override
-                                    public void log(String message) {
-                                        System.out.println("Retrofiit源码阅读: " + message);
-                                    }
-                                })
-                                        .setLevel(HttpLoggingInterceptor.Level.BODY))
-                                .build())
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                        .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://test.ph.jikecash.com")
+                .client(new OkHttpClient()
+                        .newBuilder()
+                        .addInterceptor(new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+                            @Override
+                            public void log(String message) {
+                                System.out.println("Retrofiit源码阅读: " + message);
+                            }
+                        })
+                                .setLevel(HttpLoggingInterceptor.Level.BODY))
+                        .build())
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
 
-
-        GitHubService service = retrofit.create(GitHubService.class);
-        Call<Object> call = service.listRepos("octocat");
-//        try {
-//            Response<Object> execute = call.execute();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        call.enqueue(new Callback<Object>() {
+        MyService service = retrofit.create(MyService.class);
+        Observable<Object> userApi = service.getUser("XXX");
+        userApi.subscribe(new Observer<Object>() {
             @Override
-            public void onResponse(Call<Object> call, Response<Object> response) {
+            public void onSubscribe(Disposable d) {
 
             }
 
             @Override
-            public void onFailure(Call<Object> call, Throwable t) {
+            public void onNext(Object value) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
 
             }
         });
