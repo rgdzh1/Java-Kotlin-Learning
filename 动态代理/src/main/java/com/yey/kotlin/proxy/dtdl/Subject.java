1f880e2d5fb350d7e9f48c0db29e83cdcd5689d1
Yey <rgdzh1@gmail.com>
2020-06-10 19:25:31 +0800

    删除没用的资源

--- 动态代理/src/main/java/com/yey/kotlin/proxy/dtdl/Subject.java
+++
@@ -1,7 +0,0 @@
-package com.yey.kotlin.proxy.dtdl;
-// 代理接口
-public interface Subject {
-    // 代理类与目标类都要执行的操作
-    void shopping();
-    void run();
-}
--- 动态代理/src/main/java/com/yey/kotlin/proxy/jtdl/Subject.java
+++
@@ -1,6 +0,0 @@
-package com.yey.kotlin.proxy.jtdl;
-// 代理接口
-public abstract class Subject {
-    // 代理类与目标类共同实现的方法
-    protected  abstract void operation();
-}
